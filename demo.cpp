6c0cd29aca128908a8700284d4c265ef5dae1263
slimroot <pradeep.community@gmail.com>
2025-10-25 07:17:53 +0530

    Syntax Corrected

---
+++ demo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!";
+
+    float f{2343.343f};
+    double d{23.23};
+    long double ld{};
+
+    std::cout << "Double: " << d << std::endl;
+    std::cout << "Long Double: " << ld << std::endl;
+
+    return 0;
+}
